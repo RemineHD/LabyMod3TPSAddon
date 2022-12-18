d8441534a5da392ef914c665da1cd1ac24f7555f
remine <remine@zaroz.es>
2022-12-17 19:15:54 -0800

    Code Upload
    Added all the classes and addon.json

---
+++ src/main/java/dev/remine/laby/ServerTPS.java
@@ -0,0 +1,74 @@
+package dev.remine.laby;
+
+import com.google.common.collect.EvictingQueue;
+import dev.remine.laby.modules.TPSModule;
+import net.labymod.api.LabyModAddon;
+import net.labymod.api.event.Subscribe;
+import net.labymod.api.event.events.network.IncomingPacketEvent;
+import net.labymod.api.event.events.network.server.PostLoginServerEvent;
+import net.labymod.settings.elements.SettingsElement;
+import net.minecraft.network.play.server.SUpdateTimePacket;
+
+import java.util.List;
+
+/*
+Created by elremineh#6498
+Thanks to FliegendeWurst for providing the maths to format the TPS.
+ */
+public class ServerTPS extends LabyModAddon {
+    public static EvictingQueue<Float> serverTPS = EvictingQueue.create(3);
+    private static long systemTime = 0;
+    private static long serverTime = 0;
+
+    @Override
+    public void onEnable() {
+        getApi().getEventService().registerListener(this);
+        getApi().registerModule(new TPSModule());
+    }
+
+    @Override
+    public void loadConfig() {
+
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+    }
+
+    @Subscribe
+    public void clientConnectToServer(PostLoginServerEvent postLoginServerEvent)
+    {
+        serverTPS.clear();
+        systemTime = 0;
+        serverTime = 0;
+        getApi().displayMessageInChat("Server Pre Login");
+    }
+
+    @Subscribe
+    public void serverTickEvent(IncomingPacketEvent incomingPacketEvent)
+    {
+        if (incomingPacketEvent.getPacket() instanceof SUpdateTimePacket)
+            if (systemTime == 0)
+            {
+                systemTime = System.currentTimeMillis();
+                serverTime = ((SUpdateTimePacket) incomingPacketEvent.getPacket()).getTotalWorldTime();
+            } else
+            {
+
+                long newSystemTime = System.currentTimeMillis();
+                long newServerTime = ((SUpdateTimePacket) incomingPacketEvent.getPacket()).getTotalWorldTime();
+                serverTPS.add((((float) (newServerTime - serverTime)) / (((float) (newSystemTime - systemTime)) / 50.0f)) * 20.0f);
+                systemTime = newSystemTime;
+                serverTime = newServerTime;
+            }
+    }
+
+    public static float calculateServerTPS() {
+        float sum = 0.0f;
+        for (Float f : serverTPS) {
+            sum += f;
+        }
+        return sum / (float) serverTPS.size();
+    }
+
+}
---
+++ src/main/java/dev/remine/laby/modules/TPSModule.java
@@ -0,0 +1,67 @@
+package dev.remine.laby.modules;
+
+import dev.remine.laby.ServerTPS;
+import net.labymod.ingamegui.ModuleCategory;
+import net.labymod.ingamegui.ModuleCategoryRegistry;
+import net.labymod.ingamegui.moduletypes.SimpleModule;
+import net.labymod.settings.elements.ControlElement;
+import net.labymod.utils.Material;
+
+public class TPSModule extends SimpleModule {
+
+    @Override
+    public ModuleCategory getCategory()
+    {
+        return ModuleCategoryRegistry.CATEGORY_INFO;
+    }
+
+    @Override
+    public ControlElement.IconData getIconData() {
+        return new ControlElement.IconData(Material.CLOCK);
+    }
+
+    @Override
+    public double getHeight() {
+        return 100;
+    }
+
+    @Override
+    public double getWidth() {
+        return 20;
+    }
+
+    @Override
+    public void loadSettings() {
+
+    }
+
+    @Override
+    public String getSettingName() {
+        return "Server TPS";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Test";
+    }
+
+    @Override
+    public int getSortingId() {
+        return 0;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Server TPS";
+    }
+
+    @Override
+    public String getDisplayValue() {
+        return String.format("%.1f", ServerTPS.calculateServerTPS());
+    }
+
+    @Override
+    public String getDefaultValue() {
+        return "?";
+    }
+}
